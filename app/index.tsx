77b0a00ac1b3a985bdea5a414258a0d129b79b22
Daniel Leith <elixerprince@gmail.com>
2026-03-19 19:40:21 -0500

    chore: initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-red-500 font-bold">
+        This text gets edited in real time!
+      </Text>
+    </View>
+  );
+}
