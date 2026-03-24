f677f401d04e9f7b8ec394693f1c97b924992e0b
Daniel Leith <elixerprince@gmail.com>
2026-03-24 15:41:47 -0500

    feat: create auth directory

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const SignInPage = () => {
+  return (
+    <View>
+      <Text>SignInScreen</Text>
+    </View>
+  );
+};
+
+export default SignInPage;
